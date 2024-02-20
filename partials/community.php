07b42b88c2f52226c1b15cdfc3d4995614f61d54
Allan Maia <allanmsantos51@hotmail.com>
2024-02-20 12:09:06 -0300

    Uptade 2.0

---
+++ partials/community.php
@@ -0,0 +1,18 @@
+<section class="cta-section text-center py-5 theme-bg-dark position-relative">
+    <div class="theme-bg-shapes-right"></div>
+    <div class="theme-bg-shapes-left"></div>
+    <div class="container">
+        <h3 class="mb-2 text-white mb-3">Explore nossa comunidade</h3>
+        <div class="section-intro text-white mb-3 single-col-max mx-auto">Junte-se a centenas de usuários e
+            compartilhe os mais diversos links que você tem conhecimento para ajudar e aumentar nossa comunidade.
+        </div>
+        <div class="pt-3 text-center">
+            <a class="btn btn-light position-relative" href="">Entrar na Comunidade
+                <span class="position-absolute top-0 end-0 translate-middle badge rounded-pill bg-danger">
+                    EM BREVE
+                    <span class="visually-hidden">unread messages</span>
+                </span>
+            </a>
+        </div>
+    </div>
+</section><!--//cta-section-->
---
+++ partials/hero.php
@@ -0,0 +1,14 @@
+<div class="page-header theme-bg-dark py-5 text-center position-relative">
+    <div class="theme-bg-shapes-right"></div>
+    <div class="theme-bg-shapes-left"></div>
+    <div class="container">
+        <h1 class="page-heading single-col-max mx-auto">Não sabe onde encontrar?</h1>
+        <div class="page-intro single-col-max mx-auto">Aqui você vai achar, basta procurar.</div>
+        <div class="main-search-box pt-3 d-block mx-auto">
+            <form class="search-form w-100">
+                <input type="text" placeholder="Pesquise o que você procura..." name="search" class="form-control search-input">
+                <button type="submit" class="btn search-btn" value="Search"><i class="fas fa-search"></i></button>
+            </form>
+        </div>
+    </div>
+</div><!--//page-header-->
