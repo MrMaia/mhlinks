06be6fc07985e80a42b618f58f7d35e407296e58
Allan Maia <allanmsantos51@hotmail.com>
2024-02-22 11:28:29 -0300

    update

---
+++ adm/criar_link.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+	<title>AjudaQui - Seu site dos links</title>
+
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="./coderdocs-logo.ico">
+
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&display=swap" rel="stylesheet">
+
+	<!-- FontAwesome JS-->
+	<script defer src="../assets/fontawesome/js/all.min.js"></script>
+
+	<!-- Theme CSS -->
+	<link id="theme-style" rel="stylesheet" href="../assets/css/theme.css">
+
+</head>
+
+<body>
+    <?php include '../partials/header.php'; ?>
+    teste
+</body>
